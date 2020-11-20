9abc8b9827655774fdb7ad2b44ab3028a58eab42
= <emily.haggard@exelaration.com>
2020-11-20 12:48:31 -0500

    end of day - see comments in monographs/create.vue

--- pages/monographs/_id.vue
+++ pages/monographs/_id.vue
@@ -1,13 +1,73 @@
 <template>
-  <p>Monograph details page not yet created</p>
+  <div class="monograph-properties-container">
+    <div class="monograph-name">
+      <h3>Name: {{monograph.name.toUpperCase()}}</h3>
+    </div>
+    <div class="monograph-symbol">
+      <h3>Chemical Symbol:</h3>
+      <h3 v-for="symbol in monograph.symbols" :key="symbol.index">
+        {{symbol.letters}}
+        <sub v-if="symbol.quantity > 1">{{symbol.quantity}}</sub>
+      </h3>
+    </div>
+    <div class="monograph-synonyms">
+      <h3>Synonyms: {{stringify(monograph.synonyms)}}</h3>
+    </div>
+    <div class="monograph-cas-number">
+      <h3>CAS Registry Number: {{monograph.casRegistryNumber}}</h3>
+    </div>
+    <div class="monograph-dot-shipping-name">
+      <h3>DOT Proper Shipping Name: {{monograph.dotShippingName}}</h3>
+    </div>
+    <div class="monograph-dot-hazard-class">
+      <h3>DOT Hazard Class: {{monograph.dotHazardClass}}</h3>
+    </div>
+    <div class="monograph-dot-label">
+      <h3>DOT Label: {{monograph.Label}}</h3>
+    </div>
+    <div class="tc-shipping-name">
+      <h3>TC_Shipping Name: {{monograph.tcShippingName}}</h3>
+    </div>
+    <div class="tc-classification">
+      <h3>TC Classification: {{monograph.tcClassification}}</h3>
+    </div>
+    <div class="tc-label">
+      <h3>TC Label: {{monograph.tcLabel}}</h3>
+    </div>
+    <div class="un-number">
+      <h3>UN Number: {{monograph.unNumber}}</h3>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      monograph: this.$store.state.monographs.list.filter(
+        (value, index, arr) => {
+          return value.id == this.$route.params.id;
+        }
+      )[0]
+    };
+  },
+  methods: {
+    stringify(list) {
+      if (!Array.isArray(list)) return list;
+      let result = "";
+      for (let i = 0; i < list.length; i++) {
+        if (i < list.length - 1) result += list[i] + ", ";
+        else result += list[i];
+      }
+      return result;
+    }
+  }
+};
 </script>
 
 <style>
-
+h2,
+h3 {
+  display: inline;
+}
 </style>
--- store/monographs.js
+++ store/monographs.js
@@ -1,31 +1,41 @@
 export const state = () => ({
     list: [
-        { symbols: [{ letters: "Ar", quantity: 1 }], name: "Argon", id: "6" },
-        { symbols: [{ letters: "N", quantity: 2 }], name: "Nitrogen", id: "5" },
-        { symbols: [{ letters: "O", quantity: 2 }], name: "Oxygen", id: "1" },
+        { symbols: [{ letters: "Ar", quantity: 1 }], name: "Argon", id: "1" },
+        { symbols: [{ letters: "N", quantity: 2 }], name: "Nitrogen", id: "2" },
+        {
+            symbols: [{ letters: "O", quantity: 2 }], name: "Oxygen", id: "3",
+            synonyms: ['LOX (liquid only)'], casRegistryNumber: '7782-44-7',
+            dotHazardClass: '2.2, (5.1), (Nonflammable gas)',
+            dotLabel: 'NON-FLAMMABLE GAS, OXIDIZER',
+            tcShippingName: 'Oxygen, compressed Oxygen, refrigerated liquid',
+            tcClassification: '2.2, (5.1)',
+            tcLabel: 'OXIDIZING GAS',
+            unNumber: 'UN 1072 (for compressed gas) UN 1073 (for refrigerated liquid)'
+        },
         {
             symbols: [{ letters: "COCl", quantity: 2 }],
             name: "Phosgene",
-            id: "2"
+            id: "4",
+            synonyms: ['emily', 'haggard']
         }]
 })
 
 export const mutations = {
-    create(state, symbolArray, name, id) {
-        if (!Array.isArray(symbolArray)) return false;
-        if (!Number.isInteger(id)) return false;
-        if (name == null) return false;
-        for (let i = 0; i < symbolArray.length; i++) {
-            if (symbolArray[i].letters == null) return false;
-            if (!Number.isInteger(symbolArray[i].quantity)
-                || symbolArray[i].quantity < 1) return false;
+    create(state, monograph) {
+        if (!monograph) return;
+        if (!Array.isArray(monograph.symbolArray)) return;
+        if (!Number.isInteger(monograph.id)) return;
+        if (monograph.name == null) return;
+        for (let i = 0; i < monograph.symbolArray.length; i++) {
+            if (monograph.symbolArray[i].letters == null) return;
+            if (!Number.isInteger(monograph.symbolArray[i].quantity)
+                || monograph.symbolArray[i].quantity < 1) return;
         }
-        state.list.push({ symbols: symbolArray, name: name, id: id });
-        return true;
+        state.list.push(monograph);
     },
-    update(state, symbolArray, name, id) {
-        this.delete(state, id);
-        this.create(state, symbolArray, name, id);
+    update(state, monograph) {
+        this.delete(state, monograph.id);
+        this.create(state, monograph);
     },
     delete(state, id) {
         state.list = state.list.filter((value, index, arr) => {
